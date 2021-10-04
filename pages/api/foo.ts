de1ec38e8b5fda8abc04f9654848f5922be28dae
Richard Propst <richard.propst@sentry.io>
2021-10-04 12:01:08 -0700

    Adding new API

---
+++ pages/api/foo.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { withSentry } from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    throw new Error("API throw error test");
+    res.status(200).json({ result: "bar" });
+  }
+  catch (ex) {
+    console.log('caught exception')
+    Sentry.captureException(ex)
+    res.json({});
+    res.status(500);
+    res.end();
+  }
+};
+
+export default withSentry(handler);
