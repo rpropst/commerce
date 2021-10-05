0239ef0d5eae5483703fd1864d0c94225aa11274
Richard Propst <richard.propst@sentry.io>
2021-10-05 08:48:02 -0700

    Add error handler to foo

--- pages/api/foo.ts
+++ pages/api/foo.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { withSentry } from "@sentry/nextjs";
-// import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -10,7 +10,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
   catch (ex) {
     console.log('caught exception')
-    // Sentry.captureException(ex)
+    Sentry.captureException(ex)
     res.json({});
     res.status(500);
     res.end();
